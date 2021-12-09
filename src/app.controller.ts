5977f67b3f3a469f845b2dfc86a4cb4598598fd8
rupcr <ronny.umana@inec.go.cr>
2021-12-09 15:18:50 -0600

    Swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiParam } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { ArticuloDTO } from './catalogo.dto';
 
@@ -14,11 +15,16 @@ export class AppController {
   }
 
   @Get()
+  @ApiBody({required:true, type: ArticuloDTO})
+  @ApiOperation({summary: 'Devuelve toda la consulta'})
   getTodo() {
     return this.appService.getTodo();
   }
 
   @Get("/:id")
+  @ApiBody({required:true, type: ArticuloDTO})
+  @ApiOperation({summary: 'Devuelve un articulo'})
+  @ApiParam({name: 'id'})
   getUnArticulo(@Param('id') id:string) {
     console.log(id);
     return this.appService.getUnArticulo(id);
@@ -26,19 +32,27 @@ export class AppController {
 
   @Post()
   @UsePipes(ValidationPipe)
+  @ApiBody({required:true, type: ArticuloDTO})
+  @ApiOperation({summary: 'Agregar un articulo'})
   agregar(@Body() req:any){
   return this.appService.agregarUnArticulo({});
   }
 
   @Patch('/:id')
   @UsePipes(ValidationPipe)
+  @ApiBody({required:true, type: ArticuloDTO})
+  @ApiOperation({summary: 'Editar un articulo'})
+  @ApiParam({name: 'id'})
   editar(@Param('id') id:string,@Body()data:ArticuloDTO)
   {
     console.log(id);
   	return this.appService.editarUnArticulo(id,data);
   }
 
-  @Delete("borrarArticulo/:id")
+  @Delete("/:id")
+  @ApiBody({required:true, type: ArticuloDTO})
+  @ApiOperation({summary: 'Eliminar un articulo'})
+  @ApiParam({name: 'id'})
   eliminarUnArticulo(@Param('id') id:string) {
     console.log(id);
     return this.appService.eliminarUnArticulo(id);
@@ -47,3 +61,4 @@ export class AppController {
 
 
 }
+
